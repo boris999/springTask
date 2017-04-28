f7125af4d71fd340e5f0ac5223459b0b054d6a67
Boris Atanasov <Boris_Atanasov@epam.com>
2017-04-28 09:40:25 +0300

    concurrent modification exception currently

--- Top10Words/src/words/ShowTop.java
+++ Top10Words/src/words/ShowTop.java
@@ -13,43 +13,18 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ShowTop {
-
+	private static Pattern pattern = Pattern.compile("(\\s-\\s)|\\b[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]|[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]+\\b");
 	public static void main(String[] args) throws IOException {
 		int howMany = Integer.parseInt(args[0]);
 		String fileLocation = args[1];
 		List<String> lines = Files.readAllLines(Paths.get(fileLocation));
 
 		lines.stream()
-				.filter(s -> !s.trim().equals(""))
-				.flatMap(s -> Arrays.stream(splitWords(s)))
-				// .map(s -> transform(s))
+				.flatMap(s -> Arrays.stream(pattern.split((s.replaceAll("\\.[[^\\d]&&[^\\s]]", "")))))
 				.filter(s -> !s.trim().equals(""))
 				.collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet().stream()
 				.sorted((s1, s2) -> s2.getValue().compareTo(s1.getValue())).limit(howMany)
-				.forEach(s -> System.out.println(s.getKey()));
-		// String test = "As the ‘USS Carl Vinson’ moves closer towards
-		// Pyongyang, the official newspaper 1234 of North Korea's ruling
-		// Worker's party, called the aircraft carrier's deployment - “an
-		// extremely dangerous, act by those who plan a nuclear war to invade”";
-		// System.out.println(transform2(test));
-
+				.forEach(s -> System.out.println(s.getKey()+ " "+s.getValue()));
 	}
 
-	private static String transform(String s) {
-
-		String regex = "(\\s-\\s)|\\b[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]|[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]\\b";
-		return s.replaceAll(regex, " ");
-	}
-
-	private static String[] splitWords(String s) {
-		String regex = "(\\s-\\s)|\\b[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]|[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]{1,4}\\b";
-		s=s.replaceAll("\\.", "");
-		String[] temp = s.split("\\b[[^\\p{L}]&&[^\\p{Lu}]&&[^\\d]&&[^\u0027]]{1,4}\\b");
-		String[]temp2 = s.split(regex);
-		for (String currentString : temp2) {
-			System.out.print(currentString+" ");
-		}System.out.println();
-		return temp;
-
-	}
 }
