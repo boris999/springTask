1db7c75b818f6594b702aca9c678294cb6dabde0
Boris Atanasov <Boris_Atanasov@epam.com>
2017-09-27 10:05:47 +0300

    1

---
+++ HometaskSpring/src/main/java/com/epam/spring/hometask/domain/Auditorium.java
@@ -0,0 +1,90 @@
+package com.epam.spring.hometask.domain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+/**
+ * @author Yuriy_Tkach
+ */
+public class Auditorium {
+
+	private String name;
+
+	private long numberOfSeats;
+
+	private Set<Long> vipSeats = Collections.emptySet();
+
+	public Auditorium() {
+	}
+
+	/**
+	 * Counts how many vip seats are there in supplied <code>seats</code>
+	 *
+	 * @param seats
+	 *            Seats to process
+	 * @return number of vip seats in request
+	 */
+	public long countVipSeats(Collection<Long> seats) {
+		return seats.stream().filter(seat -> this.vipSeats.contains(seat)).count();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getNumberOfSeats() {
+		return this.numberOfSeats;
+	}
+
+	public void setNumberOfSeats(long numberOfSeats) {
+		this.numberOfSeats = numberOfSeats;
+	}
+
+	public Set<Long> getAllSeats() {
+		return LongStream.range(1, this.numberOfSeats + 1).boxed().collect(Collectors.toSet());
+	}
+
+	public Set<Long> getVipSeats() {
+		return this.vipSeats;
+	}
+
+	public void setVipSeats(Set<Long> vipSeats) {
+		this.vipSeats = vipSeats;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		Auditorium other = (Auditorium) obj;
+		if (this.name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!this.name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ HometaskSpring/src/main/java/com/epam/spring/hometask/domain/User.java
@@ -0,0 +1,93 @@
+package com.epam.spring.hometask.domain;
+
+import java.util.NavigableSet;
+import java.util.Objects;
+import java.util.TreeSet;
+
+/**
+ * @author Yuriy_Tkach
+ */
+public class User extends DomainObject {
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private NavigableSet<Ticket> tickets = new TreeSet<>();
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public NavigableSet<Ticket> getTickets() {
+		return this.tickets;
+	}
+
+	public void setTickets(NavigableSet<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.firstName, this.lastName, this.email);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		User other = (User) obj;
+		if (this.email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!this.email.equals(other.email)) {
+			return false;
+		}
+		if (this.firstName == null) {
+			if (other.firstName != null) {
+				return false;
+			}
+		} else if (!this.firstName.equals(other.firstName)) {
+			return false;
+		}
+		if (this.lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		} else if (!this.lastName.equals(other.lastName)) {
+			return false;
+		}
+		return true;
+	}
+
+}
