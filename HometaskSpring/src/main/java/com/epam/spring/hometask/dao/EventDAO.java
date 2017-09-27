d1df6923f3c0f24f72f80170a2e0f89c692b939f
Boris Atanasov <Boris_Atanasov@epam.com>
2017-09-27 10:09:02 +0300

    1

--- HometaskSpring/src/main/java/com/epam/spring/hometask/dao/EventDAO.java
+++ HometaskSpring/src/main/java/com/epam/spring/hometask/dao/EventDAO.java
@@ -1,11 +1,5 @@
 package com.epam.spring.hometask.dao;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import com.epam.spring.homework.domain.Event;
-
 public class EventDAO {
-	private static Map<Event, Event> events = new ConcurrentHashMap<>();
 
 }
